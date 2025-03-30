9a33dbc3f7ee199a1dea6f16feeaaee373aa467d
Alberto Galvez <agmalpartida@gmail.com>
2025-03-30 16:30:59 +0200

    Automatic commit: 20250330-16:30

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -7,10 +7,8 @@
 ---@type LazySpec
 return {
 
-  -- == Examples of Adding Plugins ==
-
   {
-  "max397574/better-escape.nvim",
+    "max397574/better-escape.nvim",
     cond = not vim.g.vscode,
     opts = function(_, opts)
       local esc_fn = function()
@@ -52,7 +50,7 @@ return {
   },
 
   {
-  "HakonHarnes/img-clip.nvim",
+    "HakonHarnes/img-clip.nvim",
     opts = function(_, opts)
       opts.default = {
         -- file and directory options
@@ -76,28 +74,28 @@ return {
     end,
   },
 
-  -- "L3MON4D3/LuaSnip",
-  -- {
-  --   config = function(plugin, opts)
-  --     require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-  --     -- add more custom luasnip configuration such as filetype extend or custom snippets
-  --     local luasnip = require "luasnip"
-  --     vim.keymap.set({ "i", "s" }, "<C-k>", function()
-  --       if luasnip.choice_active() then luasnip.change_choice(1) end
-  --     end)
-  --     vim.keymap.set({ "i", "s" }, "<C-j>", function()
-  --       if luasnip.choice_active() then luasnip.change_choice(-1) end
-  --     end)
-  --     luasnip.filetype_extend("javascript", { "javascriptreact" })
-  --     -- load snippets paths
-  --     require("luasnip.loaders.from_vscode").lazy_load {
-  --       paths = { vim.fn.stdpath "config" .. "/snippets" },
-  --     }
-  --   end,
-  -- },
+  {
+    "L3MON4D3/LuaSnip",
+    config = function(plugin, opts)
+      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
+      -- add more custom luasnip configuration such as filetype extend or custom snippets
+      local luasnip = require "luasnip"
+      vim.keymap.set({ "i", "s" }, "<C-k>", function()
+        if luasnip.choice_active() then luasnip.change_choice(1) end
+      end)
+      vim.keymap.set({ "i", "s" }, "<C-j>", function()
+        if luasnip.choice_active() then luasnip.change_choice(-1) end
+      end)
+      luasnip.filetype_extend("javascript", { "javascriptreact" })
+      -- load snippets paths
+      require("luasnip.loaders.from_vscode").lazy_load {
+        paths = { vim.fn.stdpath "config" .. "/snippets" },
+      }
+    end,
+  },
 
   {
-  "nvim-neo-tree/neo-tree.nvim",
+    "nvim-neo-tree/neo-tree.nvim",
     opts = {
       default_component_configs = {
         container = {
@@ -151,7 +149,7 @@ return {
   },
 
   {
-  "windwp/nvim-autopairs",
+    "windwp/nvim-autopairs",
     config = function(plugin, opts)
       require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
       -- add more custom autopairs configuration such as custom rules
@@ -181,7 +179,7 @@ return {
   },
 
   {
-  "kylechui/nvim-surround",
+    "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     opts = {
@@ -197,21 +195,21 @@ return {
     },
   },
 
-  -- "goolord/alpha-nvim",
-  -- {
-  --   plugins = {
-  --     telescope = {
-  --       pickers = {
-  --         find_files = {
-  --           hidden = true,
-  --         },
-  --       },
-  --     },
-  --   },
-  -- },
+  {
+    "goolord/alpha-nvim",
+    plugins = {
+      telescope = {
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
+      },
+    },
+  },
 
   {
-  "folke/which-key.nvim",
+    "folke/which-key.nvim",
     opts = {
       preset = "modern",
       -- delay = 0,
@@ -220,7 +218,6 @@ return {
       },
     },
   },
-  -- == Examples of Overriding Plugins ==
 
   -- customize dashboard options
   {
@@ -243,51 +240,6 @@ return {
           }, "\n"),
         },
       },
-    },
-  },
-
-  -- You can disable default plugins as follows:
-  -- { "max397574/better-escape.nvim", enabled = true },
-
-  -- You can also easily customize additional setup of plugins that is outside of the plugin's setup call
-  {
-    "L3MON4D3/LuaSnip",
-    config = function(plugin, opts)
-      require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
-      -- add more custom luasnip configuration such as filetype extend or custom snippets
-      local luasnip = require "luasnip"
-      luasnip.filetype_extend("javascript", { "javascriptreact" })
-    end,
-    -- },
-
-    {
-      "windwp/nvim-autopairs",
-      config = function(plugin, opts)
-        require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) -- include the default astronvim config that calls the setup call
-        -- add more custom autopairs configuration such as custom rules
-        local npairs = require "nvim-autopairs"
-        local Rule = require "nvim-autopairs.rule"
-        local cond = require "nvim-autopairs.conds"
-        npairs.add_rules(
-          {
-            Rule("$", "$", { "tex", "latex" })
-              -- don't add a pair if the next character is %
-              :with_pair(cond.not_after_regex "%%")
-              -- don't add a pair if  the previous character is xxx
-              :with_pair(
-                cond.not_before_regex("xxx", 3)
-              )
-              -- don't move right when repeat character
-              :with_move(cond.none())
-              -- don't delete if the next character is xx
-              :with_del(cond.not_after_regex "xx")
-              -- disable adding a newline when you press <cr>
-              :with_cr(cond.none()),
-          },
-          -- disable for .vim files, but it work for another filetypes
-          Rule("a", "a", "-vim")
-        )
-      end,
     },
   },
 }
