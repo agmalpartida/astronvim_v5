29c2d56e830ac99223e2422064b758c75b2e9031
Alberto Galvez <agmalpartida@gmail.com>
2025-03-30 16:10:53 +0200

    Automatic commit: 20250330-16:10

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -96,60 +96,59 @@ return {
   --   end,
   -- },
 
-  -- "nvim-neo-tree/neo-tree.nvim",
-  -- {
-  --   opts = {
-  --     --window = 40,
-  --     -- default_component_configs = {
-  --     --   container = {
-  --     --     enable_character_fade = false,
-  --     --   },
-  --     --   -- file_size = {
-  --     --   --   enabled = true,
-  --     --   --   required_width = 24, -- min width of window required to show this column
-  --     --   -- },
-  --     --   -- type = {
-  --     --   --   enabled = true,
-  --     --   --   required_width = 122, -- min width of window required to show this column
-  --     --   -- },
-  --     --   -- last_modified = {
-  --     --   --   enabled = true,
-  --     --   --   required_width = 88, -- min width of window required to show this column
-  --     --   -- },
-  --     --   -- created = {
-  --     --   --   enabled = true,
-  --     --   --   required_width = 110, -- min width of window required to show this column
-  --     --   -- },
-  --     --   -- symlink_target = {
-  --     --   --   enabled = false,
-  --     --   -- },
-  --     -- },
-  --     -- window = {
-  --     --   position = "left",
-  --     --   width = 30,
-  --     --   mapping_options = {
-  --     --     noremap = true,
-  --     --     nowait = true,
-  --     --   },
-  --     -- },
-  --     -- filesystem = {
-  --     --   follow_current_file = {
-  --     --     enabled = true,
-  --     --   },
-  --     --   filtered_items = {
-  --     --     visible = true,
-  --     --     hide_dotfiles = false,
-  --     --     hide_gitignore = false,
-  --     --     hide_by_name = {
-  --     --       ".DS_Store",
-  --     --       "thumbs.db",
-  --     --       "node_modules",
-  --     --       "__pycache__",
-  --     --     },
-  --     --   },
-  --     -- },
-  --   },
-  -- },
+  {
+  "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      default_component_configs = {
+        container = {
+          enable_character_fade = false,
+        },
+        -- file_size = {
+        --   enabled = true,
+        --   required_width = 24, -- min width of window required to show this column
+        -- },
+        -- type = {
+        --   enabled = true,
+        --   required_width = 122, -- min width of window required to show this column
+        -- },
+        -- last_modified = {
+        --   enabled = true,
+        --   required_width = 88, -- min width of window required to show this column
+        -- },
+        -- created = {
+        --   enabled = true,
+        --   required_width = 110, -- min width of window required to show this column
+        -- },
+        symlink_target = {
+          enabled = false,
+        },
+      },
+      window = {
+        position = "left",
+        width = 35,
+        mapping_options = {
+          noremap = true,
+          nowait = true,
+        },
+      },
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+        },
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignore = false,
+          hide_by_name = {
+            ".DS_Store",
+            "thumbs.db",
+            "node_modules",
+            "__pycache__",
+          },
+        },
+      },
+    },
+  },
 
   -- "windwp/nvim-autopairs",
   -- {
