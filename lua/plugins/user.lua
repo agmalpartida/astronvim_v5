68e7b4e6d185a2e846c4787346765f77522a7829
Alberto Galvez <me@albertogalvez.com>
2025-03-31 09:51:05 +0200

    Automatic commit: 20250331-09:50

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -123,7 +123,7 @@ return {
       },
       window = {
         position = "left",
-        width = 35,
+        width = 30,
         mapping_options = {
           noremap = true,
           nowait = true,
