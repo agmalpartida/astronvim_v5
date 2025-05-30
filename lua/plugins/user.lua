12ea479e413e451a016187488288303364413a7b
Alberto Galvez <agmalpartida@gmail.com>
2025-05-30 12:50:57 +0200

    Automatic commit: 20250530-12:50

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -94,6 +94,42 @@ return {
     end,
   },
 
+ require('winbar').setup({
+    enabled = true,
+
+    show_file_path = true,
+    show_symbols = true,
+
+    colors = {
+        path = '', -- You can customize colors like #c946fd
+        file_name = '',
+        symbols = '',
+    },
+
+    icons = {
+        file_icon_default = '',
+        seperator = '>',
+        editor_state = '●',
+        lock_icon = '',
+    },
+
+    exclude_filetype = {
+        'help',
+        'startify',
+        'dashboard',
+        'packer',
+        'neogitstatus',
+        'NvimTree',
+        'Trouble',
+        'alpha',
+        'lir',
+        'Outline',
+        'spectre_panel',
+        'toggleterm',
+        'qf',
+    }
+}) 
+  
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
