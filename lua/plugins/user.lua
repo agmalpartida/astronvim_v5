56f791b6df7de21415f4f4bc654732090469a1ad
Alberto Galvez <agmalpartida@gmail.com>
2025-05-30 13:00:57 +0200

    Automatic commit: 20250530-13:00

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -75,6 +75,48 @@ return {
   },
 
   {
+    "fgheng/winbar.nvim",
+    opts = function(_, opts)
+      opts.default = {
+       enabled = true,
+
+       show_file_path = true,
+       show_symbols = true,
+
+       colors = {
+           path = '#c946fd', -- You can customize colors like #c946fd
+           file_name = '',
+           symbols = '',
+       },
+
+       icons = {
+           file_icon_default = '',
+           seperator = '>',
+           editor_state = '●',
+           lock_icon = '',
+       },
+
+       exclude_filetype = {
+           'help',
+           'startify',
+           'dashboard',
+           'packer',
+           'neogitstatus',
+           'NvimTree',
+           'Trouble',
+           'alpha',
+           'lir',
+           'Outline',
+           'spectre_panel',
+           'toggleterm',
+           'qf',
+       }
+
+      }
+    end,
+  },
+
+  {
     "L3MON4D3/LuaSnip",
     config = function(plugin, opts)
       require "astronvim.plugins.configs.luasnip"(plugin, opts) -- include the default astronvim config that calls the setup call
@@ -94,42 +136,6 @@ return {
     end,
   },
 
- require('winbar').setup({
-    enabled = true,
-
-    show_file_path = true,
-    show_symbols = true,
-
-    colors = {
-        path = '', -- You can customize colors like #c946fd
-        file_name = '',
-        symbols = '',
-    },
-
-    icons = {
-        file_icon_default = '',
-        seperator = '>',
-        editor_state = '●',
-        lock_icon = '',
-    },
-
-    exclude_filetype = {
-        'help',
-        'startify',
-        'dashboard',
-        'packer',
-        'neogitstatus',
-        'NvimTree',
-        'Trouble',
-        'alpha',
-        'lir',
-        'Outline',
-        'spectre_panel',
-        'toggleterm',
-        'qf',
-    }
-}) 
-  
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
