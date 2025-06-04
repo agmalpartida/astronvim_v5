8b3ed8599759c2139330e5334f777b6a1c6ae81f
Alberto Galvez <me@albertogalvez.com>
2025-06-04 23:10:58 +0200

    Automatic commit: 20250604-23:10

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -59,5 +59,29 @@ return {
         )
       end)
     )
+
+    local FileName = {
+    provider = function(self)
+        -- first, trim the pattern relative to the current directory. For other
+        -- options, see :h filename-modifers
+        local filename = vim.fn.fnamemodify(self.filename, ":.")
+        if filename == "" then return "[No Name]" end
+        -- now, if the filename would occupy more than 1/4th of the available
+        -- space, we trim the file path to its initials
+        -- See Flexible Components section below for dynamic truncation
+        if not conditions.width_percent_below(#filename, 0.25) then
+            filename = vim.fn.pathshorten(filename)
+        end
+        return filename
+    end,
+    hl = { fg = utils.get_highlight("Directory").fg },
+    }
+-- let's add the children to our FileNameBlock component
+FileNameBlock = utils.insert(FileNameBlock,
+    FileIcon,
+    utils.insert(FileNameModifer, FileName), -- a new table where FileName is a child of FileNameModifier
+    FileFlags,
+    { provider = '%<'} -- this means that the statusline is cut here when there's not enough space
+)
   end,
 }
