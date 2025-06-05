e713853089cbf23daee97837fcf3f192166cc84f
Alberto Galvez <agmalpartida@gmail.com>
2025-06-05 16:41:01 +0200

    Automatic commit: 20250605-16:40

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,7 @@
+-- Disabled plugins
+return {
+  { "rcarriga/nvim-dap-ui", enabled = false },
+  { "AstroNvim/astrotheme", enabled = false },
+  { "RRethy/vim-illuminate", enabled = false },
+  { "max397574/better-escape.nvim", enabled = false },
+}
