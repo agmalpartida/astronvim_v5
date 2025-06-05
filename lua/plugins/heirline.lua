c2abdbf28e31ffadea6ca1d1cbb6cba8921a95de
Alberto Galvez <agmalpartida@gmail.com>
2025-06-05 09:01:00 +0200

    Automatic commit: 20250605-09:00

---
+++ lua/plugins/heirline.lua
@@ -0,0 +1,58 @@
+return {
+  "rebelot/heirline.nvim",
+  opts = function(_, opts)
+    local status = require "astroui.status"
+
+    opts.statusline = {
+      hl = { fg = "fg", bg = "bg" },
+      status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
+      status.component.git_branch(),
+      status.component.file_info {
+        filename = {},
+        filetype = false,
+        file_modified = { padding = { left = 1, right = 1 } },
+      },
+      status.component.git_diff(),
+      status.component.diagnostics(),
+      status.component.fill(),
+      status.component.cmd_info(),
+      status.component.fill(),
+      status.component.lsp {
+        lsp_progress = false,
+        lsp_client_names = {
+          mappings = {
+            ruff = "ruff+",
+            ruff_organize_imports = "ruff+",
+            ruff_format = "ruff+",
+          },
+        },
+      },
+      status.component.treesitter(),
+      { provider = " %4l/%L:%-3c %3p%%" },
+      status.component.mode { surround = { separator = "right" } },
+    }
+
+    -- custom winbar
+    local path_func = status.provider.filename { modify = ":.:h", fallback = "" }
+    opts.winbar[1][1] = status.component.separated_path { path_func = path_func }
+    opts.winbar[2] = {
+      status.component.separated_path { path_func = path_func },
+      status.component.file_info { -- add file_info to breadcrumbs
+        file_icon = { hl = status.hl.filetype_color, padding = { left = 0 } },
+        file_modified = false,
+        file_read_only = false,
+        filename = {},
+        filetype = false,
+        hl = status.hl.get_attributes("winbar", true),
+        surround = false,
+        update = "BufEnter",
+      },
+      status.component.breadcrumbs {
+        icon = { hl = true },
+        hl = status.hl.get_attributes("winbar", true),
+        prefix = true,
+        padding = { left = 0 },
+      },
+    }
+  end,
+}
