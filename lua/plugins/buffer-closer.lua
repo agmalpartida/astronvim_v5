06ad97238b826e3b9a3c838840944577239b2be0
Alberto Galvez <me@albertogalvez.com>
2025-09-06 23:41:25 +0200

    Automatic commit: 20250906-23:40

--- lua/plugins/buffer-closer.lua
+++ lua/plugins/buffer-closer.lua
@@ -4,7 +4,7 @@ return {
   event = "VeryLazy",
   config = function()
     local settings = {
-      excluded_filetypes = { "lazy", "NvimTree", "mason" },
+      excluded_filetypes = { "lazy", "NvimTree", "mason", "neo-tree" },
       excluded_buftypes = { "terminal", "nofile", "quickfix", "prompt", "help" },
       retirement_minutes = 1,
       min_remaining_buffers = 3,
