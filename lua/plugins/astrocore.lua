4d0f2f10b1b905e14f03ef17510dbf4bef0b4641
Alberto Galvez <me@albertogalvez.com>
2025-09-16 10:11:09 +0200

    Automatic commit: 20250916-10:10

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -87,31 +87,31 @@ return {
       --     end,
       --   },
       -- },     
-      git_branch_sessions = {
-        -- auto save directory sessions on leaving
-        {
-          event = "VimLeavePre",
-          desc = "Save git branch directory sessions on close",
-          callback = vim.schedule_wrap(function()
-            if require("astrocore.buffer").is_valid_session() then
-              require("resession").save(get_session_name(), { dir = "dirsession", notify = false })
-            end
-          end),
-        },
-        -- auto restore previous previous directory session, remove if necessary
-        {
-          event = "VimEnter",
-          desc = "Restore previous directory session if neovim opened with no arguments",
-          nested = true, -- trigger other autocommands as buffers open
-          callback = function()
-            -- Only load the session if nvim was started with no args
-            if vim.fn.argc(-1) == 0 then
-              -- try to load a directory session using the current working directory
-              require("resession").load(get_session_name(), { dir = "dirsession", silence_errors = true })
-            end
-          end,
-        },
-      },
+      -- git_branch_sessions = {
+      --   -- auto save directory sessions on leaving
+      --   {
+      --     event = "VimLeavePre",
+      --     desc = "Save git branch directory sessions on close",
+      --     callback = vim.schedule_wrap(function()
+      --       if require("astrocore.buffer").is_valid_session() then
+      --         require("resession").save(get_session_name(), { dir = "dirsession", notify = false })
+      --       end
+      --     end),
+      --   },
+      --   -- auto restore previous previous directory session, remove if necessary
+      --   {
+      --     event = "VimEnter",
+      --     desc = "Restore previous directory session if neovim opened with no arguments",
+      --     nested = true, -- trigger other autocommands as buffers open
+      --     callback = function()
+      --       -- Only load the session if nvim was started with no args
+      --       if vim.fn.argc(-1) == 0 then
+      --         -- try to load a directory session using the current working directory
+      --         require("resession").load(get_session_name(), { dir = "dirsession", silence_errors = true })
+      --       end
+      --     end,
+      --   },
+      -- },
       autohidetabline = {
         -- each augroup contains a list of auto commands
         {
@@ -158,32 +158,32 @@ return {
       -- show notification on every working directory change
       notify = false,
     },
-    sessions = {
-      -- Configure auto saving
-      autosave = {
-        last = true, -- auto save last session
-        cwd = true, -- auto save session for each working directory
-      },
-      mappings = {
-        n = {
-          -- update save dirsession mapping to get the correct session name
-          ["<Leader>SS"] = {
-            function() require("resession").save(get_session_name(), { dir = "dirsession" }) end,
-            desc = "Save this dirsession",
-          },
-          -- update load dirsession mapping to get the correct session name
-          ["<Leader>S."] = {
-            function() require("resession").load(get_session_name(), { dir = "dirsession" }) end,
-            desc = "Load current dirsession",
-          },
-        },
-      },
-      ignore = {
-        dirs = {}, -- working directories to ignore sessions in
-        filetypes = { "gitcommit", "gitrebase", "node_modules" }, -- filetypes to ignore sessions
-        buftypes = {}, -- buffer types to ignore sessions
-      },
-    },
+    -- sessions = {
+    --   -- Configure auto saving
+    --   autosave = {
+    --     last = true, -- auto save last session
+    --     cwd = true, -- auto save session for each working directory
+    --   },
+    --   mappings = {
+    --     n = {
+    --       -- update save dirsession mapping to get the correct session name
+    --       ["<Leader>SS"] = {
+    --         function() require("resession").save(get_session_name(), { dir = "dirsession" }) end,
+    --         desc = "Save this dirsession",
+    --       },
+    --       -- update load dirsession mapping to get the correct session name
+    --       ["<Leader>S."] = {
+    --         function() require("resession").load(get_session_name(), { dir = "dirsession" }) end,
+    --         desc = "Load current dirsession",
+    --       },
+    --     },
+    --   },
+    --   ignore = {
+    --     dirs = {}, -- working directories to ignore sessions in
+    --     filetypes = { "gitcommit", "gitrebase", "node_modules" }, -- filetypes to ignore sessions
+    --     buftypes = {}, -- buffer types to ignore sessions
+    --   },
+    -- },
     -- Configure core features of AstroNvim
     features = {
       large_buf = { size = 1024 * 256, lines = 10000 }, -- set global limits for large files for disabling features like treesitter
