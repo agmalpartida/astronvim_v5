8305f830eded9be979f8dcd8cc61d489133e8df6
Alberto Galvez <me@albertogalvez.com>
2025-09-23 20:31:05 +0200

    Automatic commit: 20250923-20:30

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -32,15 +32,6 @@ return {
           command = "set filetype=groovy",
         },
       },
-      change_directory = {
-        {
-          event = "BufEnter",
-          desc = "Auto change directory to file's dir",
-          callback = function()
-            vim.cmd("silent! lcd %:p:h")
-          end,
-        },
-      },
       markdown_folding = {
         -- El augroup puede llamarse como quieras
         {
